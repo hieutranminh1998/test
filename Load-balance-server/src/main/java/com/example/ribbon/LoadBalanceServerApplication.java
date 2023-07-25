d15c19c0e566020c2c9904edd76e9dec32c95e03
hieutm <hieutm2@tnex.com.vn>
2023-07-25 09:35:27 +0700

    fix api gateway

--- Load-balance-server/src/main/java/com/example/ribbon/LoadBalanceServerApplication.java
+++ Load-balance-server/src/main/java/com/example/ribbon/LoadBalanceServerApplication.java
@@ -3,9 +3,12 @@ package com.example.ribbon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonAutoConfiguration;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "server", configuration = RibbonConfiguration.class)
 public class LoadBalanceServerApplication {
 
 	public static void main(String[] args) {
